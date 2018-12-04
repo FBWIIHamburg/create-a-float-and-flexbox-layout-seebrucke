7ba6710951628f05175d2c71f44a3b39c27e5baf
seebrucke <mojtaba.zargar2010@gmail.com>
2018-12-04 15:48:46 +0100

    changed

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
-function hideScroll(){
-    document.getElementsByClassName("content")[0].style="overflow-y: hidden; height:unset;"
-    var hcontent = document.getElementsByClassName("content")[0].offsetHeight;
-    var hHeader= document.getElementsByClassName('headFloat')[0].offsetHeight;
+function hideScroll() {
+
+
+    document.getElementsByClassName('content')[0].style = "overflow-y:hidden; height:unset;";
+    var hContent = document.getElementsByClassName('content')[0].offsetHeight;
+    var hHeader = document.getElementsByClassName('headFloat')[0].offsetHeight;
     var hFooter = document.getElementsByClassName('footer')[0].offsetHeight;
-
-
-    var sum = parseFloat(hContent) + parseFloat(hHeader) +parseFloat(hFooter);
-    var str= "height:" + hContent + "px";
-    var str2="height:" + sum+"px";
-    window.alert(str2);
-    document.getElementsByClassName('navFloat')[0].style=str2;
+   
+    sum = hContent + hFooter + hHeader;
+    var sumlitle = hContent + hFooter ;
+  
+    
+    var str = "height:" + sumlitle + "px";
+    var str2 = "height:" + sum + "px";
+    document.getElementsByClassName('navFloat')[0].style = str2;
+    document.getElementsByClassName('right')[0].style = str;
 
 }
